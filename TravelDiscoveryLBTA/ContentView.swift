49ef5de05a75c5c198b0bbfcc76b6f9318f8883e
HatefMas <masoumianhatef@gmail.com>
2021-05-10 16:47:07 -0700

    Editting DiscoverCategories ScrollView and extracting the subview

--- TravelDiscoveryLBTA/ContentView.swift
+++ TravelDiscoveryLBTA/ContentView.swift
@@ -13,29 +13,7 @@ struct ContentView: View {
             //Text("Hello, world!")
                 //.padding()
                 //.navigationBarTitle("Inline", displayMode: .inline)
-            ScrollView{
-                HStack(spacing: 16){
-                    Spacer()
-                        .frame(width: 50, height: 50)
-                        .background(Color.gray)
-                        .cornerRadius(.infinity)
-                    
-                    Spacer()
-                        .frame(width: 50, height: 50)
-                        .background(Color.gray)
-                        .cornerRadius(.infinity)
-                    
-                    Spacer()
-                        .frame(width: 50, height: 50)
-                        .background(Color.gray)
-                        .cornerRadius(.infinity)
-
-                }
-                
-                Text("Test")
-                
-                
-            }.navigationTitle("Discover")
+            DiscoverCategoriesView()
             
         }
         
@@ -45,5 +23,35 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct DiscoverCategoriesView: View {
+    var body: some View {
+        ScrollView{
+            
+            ScrollView(.horizontal, showsIndicators: false /* Removes scroll bar*/) {
+                HStack(spacing: 8){
+                    ForEach(0..<8, id: \.self) { num in
+                        VStack (spacing: 4){
+                            Spacer()
+                                .frame(width: 50, height: 50)
+                                .background(Color.gray)
+                                .cornerRadius(.infinity)
+                                .shadow(color: .gray, radius: 3, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 3)
+                            Text("Art")
+                                .font(.system(size: 12, weight: .semibold))
+                            
+                        }
+                        
+                    }
+                    
+                }.padding(.horizontal) // Make it so the circles dont clip to the edges
+            }
+            
+            
+            
+            
+        }.navigationTitle("Discover")
     }
 }
