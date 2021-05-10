a7383e5dfe0ffdb935f545b627ee1ea34e6de021
HatefMas <masoumianhatef@gmail.com>
2021-05-10 15:17:52 -0700

    Initial Commit

---
+++ TravelDiscoveryLBTA/TravelDiscoveryLBTAApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelDiscoveryLBTAApp.swift
+//  TravelDiscoveryLBTA
+//
+//  Created by Sahab on 5/10/21.
+//
+
+import SwiftUI
+
+@main
+struct TravelDiscoveryLBTAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
